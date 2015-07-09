fd22490d1098a96f33029e75ca81880e1c26e2c5
Torkel Ödegaard <torkel.odegaard@gmail.com>
2015-07-09 11:44:33 +0200

    Built grunt file for less watching and fixed paths to images

---
+++ hugo/Gruntfile.js
@@ -0,0 +1,85 @@
+module.exports = function(grunt) {
+
+  var AutoPrefixPlugin = require('less-plugin-autoprefix');
+  var CleanCssPlugin = require('less-plugin-clean-css');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      dev: {
+        paths: ['static/less/'],
+        src: ['static/less/raintank.less'],
+        dest: 'build/dev/css/raintank.css',
+      },
+      dist: {
+        paths: ['static/less/'],
+        src: ['static/less/raintank.less'],
+        dest: 'build/dist/css/raintank.css',
+        options: {
+          plugins: [new AutoPrefixPlugin({ browsers: ['> 0.1%'] }), new CleanCssPlugin({})]
+        }
+      },
+    },
+
+    uglify: {
+      dist: {
+        src: 'static/js/bootstrap.js',
+        dest: 'build/dist/js/bootstrap.min.js',
+      }
+    },
+
+    watch: {
+      options: {
+        atBegin: true,
+        livereload: true,
+      },
+      less: {
+        files: ['static/less/*.less'],
+        tasks: 'less:dev',
+      },
+      hugo: {
+        files: ['layouts/**', 'content/**'],
+        tasks: 'hugo:dev',
+      },
+      all: {
+        files: ['Gruntfile.js'],
+        tasks: 'dev',
+      },
+    },
+
+    connect: {
+      mysite: {
+        options: {
+          hostname: '127.0.0.1',
+          port: 1342,
+          protocol: 'http',
+          base: 'build/dev',
+          livereload: true,
+          keepalive: true,
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('hugo', function(target) {
+    var done = this.async();
+    var args = ["--destination=build/" + target];
+
+    if (target === 'dev') {
+      args.push('--baseUrl=http://127.0.0.1:1342');
+      args.push('--buildDrafts=true');
+      args.push('--buildFuture=true');
+    }
+
+    hugo = require('child_process').spawn('hugo', args, {stdio: 'inherit'});
+    hugo.on('exit', function() { done(true); });
+    hugo.on('error', function() { done(true); });
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('dev', ['less:dev', 'hugo:dev']);
+  grunt.registerTask('default', ['less:dist', 'uglify', 'hugo:dist']);
+  grunt.registerTask('edit', ['connect', 'watch']);
+};
