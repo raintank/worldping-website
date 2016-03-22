d6cb22473b756595847a85cb3129c46ff989fa9e
Trent White <trent@raintank.io>
2016-03-22 15:24:49 -0400

    swapping out logo for worldPing. Adding raintank logo to footer. Setting up nav

--- content/timezone/data.js
+++
@@ -1,1 +0,0 @@
-var data = [[-8.0,"Tacoma","PST UTC-8",true],[-5.0,"New York City","EST UTC-5",true],[1.0,"Stockholm","CET UTC+1",true],[8.0,"Perth","SGT UTC+8",true],[13.0,"Auckland","NZDT UTC+13",true]];
